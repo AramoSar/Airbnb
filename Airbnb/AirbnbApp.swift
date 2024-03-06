7a36458aac73e7cafdb71cb9dd3791054744b88b
Aram <aramsaribekyan1@gmail.com>
2024-03-06 12:16:33 +0400

    Initial Commit

---
+++ Airbnb/AirbnbApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbApp.swift
+//  Airbnb
+//
+//  Created by Aram on 06.03.24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
